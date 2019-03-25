d78a9c1bfec6934fe49c8757a0314dd7fbd30117
DELL <DELL@DESKTOP-UUAIDHE>
2019-03-25 21:18:26 +0530

    GUI

---
+++ GUI/src/main/App.java
@@ -0,0 +1,13 @@
+ package main;
+
+import view.AddCustomerWindow;
+
+public class App {
+
+	public static void main(String[] args) {
+		AddCustomerWindow window = new AddCustomerWindow();
+		window.showWindow();
+
+	}
+
+}
