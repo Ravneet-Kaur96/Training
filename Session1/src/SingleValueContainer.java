48e5cd90efc471c68a7fe186f29e83a5f96e5287
DELL <DELL@DESKTOP-UUAIDHE>
2019-03-25 19:52:36 +0530

    Single Value Container

---
+++ Session1/src/SingleValueContainer.java
@@ -0,0 +1,24 @@
+
+public class SingleValueContainer {
+
+	public static void main(String[] args) {
+		byte age = 50;
+		System.out.println("Age is: "+age);
+		//updating age
+		age = 55; 
+		System.out.println("Age now is: "+age);
+		double salary = 34000.999;
+		System.out.println("Salary is:"+salary);
+
+		char ch = 'A';
+		//ch = 66;
+		//ch = '\u20b9';
+		System.out.println("ch is: "+ch);
+
+		/*boolean isInternetConnected = true;
+		isInternetConnected = false;*/
+
+		
+	}
+
+}
